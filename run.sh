ac9d8022e2c2beaa6e392ebd8c1f1140dcf1dd49
Igor Izotov <iizotov@users.noreply.github.com>
2018-12-05 15:29:37 +1100

    fix in run.sh - initial sleep should not be a part of the execution loop

--- run.sh
+++ run.sh
@@ -8,11 +8,13 @@ var_expand() {
   eval printf '%s' "\"\${$1?}\""
 }
 
+echo "Sleeping for $INITIAL_SLEEP seconds"
+[ "$INITIAL_SLEEP" ] && /bin/sleep $INITIAL_SLEEP
+
 for ((i=1;i<=$NUM_ITERATIONS;i++))
 do
-    [ "$INITIAL_SLEEP" ] && /bin/sleep $INITIAL_SLEEP
-
     dotnet loadgenerator.dll -c "$(var_expand CONNECTION_STRING_$i)" -b $(var_expand BATCH_$i) \
         -t $(var_expand THROUGHPUT_$i) --terminate-after $(var_expand TERMINATE_AFTER_$i) \
         -j -s $(var_expand SIZE_$i) --service $(var_expand SERVICE_$i)
 done
+
